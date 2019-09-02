a0408a8536b958e7d3ca048fdb0ef721b6af610a
Jakub Dorňák <jakub.dornak@misli.cz>
2019-09-03 00:54:57 +0200

    drop unused settings

--- spolkovydumpodivin/settings.py
+++ spolkovydumpodivin/settings.py
@@ -1,11 +1,3 @@
-# -*- coding: utf-8 -*-
-
-from __future__ import unicode_literals
-
-"""
-Django settings for spolkovydumpodivin project.
-"""
-
 from django.utils.translation import ugettext_lazy as _
 from leprikon.site.settings import *
 
@@ -13,34 +5,3 @@ from leprikon.site.settings import *
 INSTALLED_APPS = [
     'cms_articles',
 ] + INSTALLED_APPS
-
-CMS_TEMPLATES = [
-    ('default.html', 'Výchozí'),
-]
-
-# templates used to render plugin article
-CMS_ARTICLES_PLUGIN_ARTICLE_TEMPLATES = [
-    ('default', 'Výchozí'),
-]
-
-# templates used to render plugin articles
-CMS_ARTICLES_PLUGIN_ARTICLES_TEMPLATES = [
-    ('default', 'Výchozí'),
-]
-
-CMS_PLACEHOLDER_CONF = {
-    'content': {
-        'name': 'Obsah',
-    },
-}
-
-THUMBNAIL_ALIASES = {
-    '': {
-        'preview': {
-            'size': (240, 10000),
-        },
-        'view': {
-            'size': (760, 570),
-        },
-    },
-}
