60480a1809b6e48457719af9193a441e044a8083
Jakub Dorňák <jakub.dornak@misli.cz>
2020-03-17 18:42:28 +0100

    Integrate RocketChat

--- spolkovydumpodivin/settings.py
+++ spolkovydumpodivin/settings.py
@@ -2,6 +2,4 @@ from django.utils.translation import ugettext_lazy as _
 from leprikon.site.settings import *
 
 # Application definition
-INSTALLED_APPS = [
-    'cms_articles',
-] + INSTALLED_APPS
+INSTALLED_APPS.append('cms_articles')
